205aabd6fc42cf48ab3eaed4a9c1b3092b3f7f14
Adam Ross <adamross@google.com>
2019-03-07 11:01:21 -0800

    working helloworld added

--- main.go
+++ main.go
@@ -1,1 +1,25 @@
-package "main"
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	log.Print("Greeting requested")
+	fmt.Fprint(w, "Hello World!")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	log.Printf("Listening on port %s", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+}
